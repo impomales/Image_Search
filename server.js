75164778c3f78c89d9e6cb031e67d3a38cddc097
Isaias <impomales@gmail.com>
2016-09-14 05:43:21 +0000

    given param, can paginate through results

--- server.js
+++ server.js
@@ -14,25 +14,35 @@ app.get('/:query', function(req, res) {
     // use request for get request to google server.
     var url = 'https://www.googleapis.com/customsearch/v1?searchType=image&key=' + APIKey + '&cx=' + CX + '&q=' + query
     
+    var offset = req.query.offset ? '&start=' + (parseInt(req.query.offset) + 1) : ''
+    
+    url += offset
+    
     request(url, function(err, response, body) {
         if (err) throw err
+        console.log(url)
+        var bodyParsed = JSON.parse(body)
         
-        var items = JSON.parse(body).items
-        var results = []
+        if (response.statusCode == 403) res.send('limit reached')
         
-        items.forEach(function(item) {
-            var result = {
-                url: item.link,
-                snippet: item.snippet,
-                thumbnail: item.image.thumbnailLink,
-                context: item.image.contextLink
-            }
-            results.push(result)
-        })
-        
-        res.json(results)
+        if (response.statusCode == 200) {
+            var items = JSON.parse(body).items
+            console.log(items)
+            var results = []
+            
+            items.forEach(function(item) {
+                var result = {
+                    url: item.link,
+                    snippet: item.snippet,
+                    thumbnail: item.image.thumbnailLink,
+                    context: item.image.contextLink
+                }
+                results.push(result)
+            })
+            
+            res.json(results)
+        }
     })
-    
 })
 
 app.get('/latest', function(req, res) {
